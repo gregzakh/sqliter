6e62d8bb38a345682ae2bccb0c6dcc34499ff5df
sqlailama <sqlailama@users.noreply.github.com>
2026-05-27 15:14:02 +0500

    M sqlite/test_db/person_tests.sql

--- sqlite/test_db/person_tests.sql
+++ sqlite/test_db/person_tests.sql
@@ -14,3 +14,23 @@ select pers_id, name, surname, birthday
 from person
 where julianday(strftime('%Y-12-31', birthday)) -
       julianday(strftime('%Y-01-01', birthday)) > 364;
+
+-- youngest namesake
+select pers_id, name||' '||surname name, birthday
+from person
+group by name, surname
+having count(*) > 1 and max(birthday);
+
+select pers_id, name||' '||surname name, birthday
+from (
+   select last_value(birthday) over(
+      partition by name, surname order by birthday
+      rows between unbounded preceding
+               and unbounded following
+   ) lv, count(*) over(
+      partition by name, surname
+   ) cnt, pers_id, name, surname, birthday
+   from person
+)
+where lv = birthday
+  and cnt > 1;
