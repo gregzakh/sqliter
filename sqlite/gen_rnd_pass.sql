9b896b7f1edaf823b94cc8940324440c082eee57
gregzakh <gregzakh@users.noreply.github.com>
2026-06-27 22:22:56 +0500

    2026-06-27 22:22:56

--- sqlite/gen_rnd_pass.sql
+++
@@ -1,7 +0,0 @@
--- simple password generator
-with chars(c) as (
-   select 33 union all select c + 1 from chars limit 90
-)
-select group_concat(c, '') from (
-   select char(c) c from chars order by random() limit 13
-);
