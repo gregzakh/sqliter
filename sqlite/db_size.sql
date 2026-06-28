e3ce32f4dae7f0a1b990d7ea64c599372e09b2eb
gregzakh <gregzakh@users.noreply.github.com>
2026-06-28 13:08:59 +0500

    2026-06-28 13:08:59

--- sqlite/db_size.sql
+++
@@ -1,9 +0,0 @@
--- computes size of the database
-with db_size(Kb) as (
-   select page_count * page_size / 1024.0
-   from pragma_page_count(), pragma_page_size()
-)
-select round(Kb, 1) Kb,
-       round(Kb / 1024, 1) Mb,
-       round(Kb / 1024 / 1024, 1) Gb
-from db_size;
